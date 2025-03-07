0c41d246a73f38d259cbf3aaf79a45bb0b4c23e5
Micheal <khanzaidan786@gmail.com>
2025-03-07 23:32:58 +0530

    Update bottom_coupon.dart

--- lib/components/bottom_coupon.dart
+++ lib/components/bottom_coupon.dart
@@ -5,82 +5,73 @@ class CouponCode extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(25.0),
-          child: Image.asset("assets/Group 10584.png"),
-        ),
-
-        Padding(
-          padding: const EdgeInsets.all(25.0),
-          child: Stack(
+    return Padding(
+      padding: const EdgeInsets.all(25.0),
+      child: Stack(
+        children: [
+          Image.asset("assets/Subtract.png"),
+          Column(
             children: [
-              Image.asset("assets/Subtract.png"),
-              Column(
+              SizedBox(height: 15),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  SizedBox(height: 15),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Text(
-                        "Extra 10% Off | ",
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 15,
-                        ),
-                      ),
-                      Text(
-                        "Use code : ",
-                        style: TextStyle(
-                          fontWeight: FontWeight.w500,
-                          fontSize: 15,
-                        ),
-                      ),
-                      Text(
-                        "ATVR25",
-                        style: TextStyle(
-                          fontWeight: FontWeight.w500,
-                          fontSize: 15,
-                          color: Colors.blue,
-                        ),
-                      ),
-                    ],
+                  Text(
+                    "Extra 10% Off | ",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                    ),
                   ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Image.asset("assets/Group 70.png", width: 15),
-                      SizedBox(width: 3),
-                      Text(
-                        "Min. Spend \$300",
-                        style: TextStyle(
-                          fontWeight: FontWeight.normal,
-                          fontSize: 15,
-                          color: Color(0xFFA2A1AB),
-                        ),
-                      ),
-                      SizedBox(width: 10),
-                      Image.asset("assets/Group 72.png", width: 15),
-                      SizedBox(width: 3),
-                      Text(
-                        "Until May 23, 2025",
-                        style: TextStyle(
-                          fontWeight: FontWeight.normal,
-                          fontSize: 15,
-                          color: Color(0xFFA2A1AB),
-                        ),
-                      ),
-                    ],
+                  Text(
+                    "Use code : ",
+                    style: TextStyle(
+                      fontWeight: FontWeight.w500,
+                      fontSize: 20,
+                    ),
+                  ),
+                  Text(
+                    "ATVR25",
+                    style: TextStyle(
+                      fontWeight: FontWeight.w500,
+                      fontSize: 20,
+                      color: Colors.blue,
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Image.asset("assets/Group 70.png", width: 15),
+                  SizedBox(width: 3),
+                  Text(
+                    "Min. Spend \$300",
+                    style: TextStyle(
+                      fontWeight: FontWeight.normal,
+                      fontSize: 15,
+                      color: Color(0xFFA2A1AB),
+                    ),
+                  ),
+                  SizedBox(width: 15),
+                  Image.asset("assets/Group 72.png", width: 15),
+                  SizedBox(width: 3),
+                  Text(
+                    "Until May 23, 2025",
+                    style: TextStyle(
+                      fontWeight: FontWeight.normal,
+                      fontSize: 15,
+                      color: Color(0xFFA2A1AB),
+                    ),
                   ),
                 ],
               ),
             ],
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
